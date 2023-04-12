db669c8ff9118d460e8bde75055e492c87482c55
nomin-sasabuchi <nomin.sasabuchi@karabiner.tech>
2023-04-12 18:53:08 +0900

    CI修正

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // `root` からの相対パスで指定する
+    outDir: './',
+  },
 })
