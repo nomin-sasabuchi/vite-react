8c45a9e8f51bd54970151dafbc16f0b1a17f769e
nomin-sasabuchi <nomin.sasabuchi@karabiner.tech>
2023-04-12 12:04:17 +0900

    CIテスト

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import ReactDOM, { createRoot } from 'react-dom/client'
 import './index.css'
+import * as Sentry from "@sentry/react";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+Sentry.init({
+  dsn: "https://ba0352ba1c3040c3931bad124db031a0@o4504954186563584.ingest.sentry.io/4504954189643776",
+});
+
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+  root.render(<App />)
+}
