f073d0b5f35b0c5d2733a2dffc0bee21680ee0a4
nomin-sasabuchi <nomin.sasabuchi@karabiner.tech>
2023-04-14 13:59:12 +0900

    CI調整

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -8,7 +8,7 @@ Sentry.init({
   dsn: import.meta.env.VITE_SENTRY_DSN,
   integrations: [new Sentry.BrowserTracing()],
   tracesSampleRate: 1.0,
-  release: import.meta.env.VITE_SENTRY_RELEASE_VERSION,
+  release: import.meta.env.VITE_SENTRY_RELEASE_VERSION && import.meta.env.VITE_SENTRY_RELEASE_VERSION,
 });
 
 
