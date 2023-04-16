1c3b66949dc11aea6c47196f64c2f4d3442b85a1
nomin-sasabuchi <nomin.sasabuchi@karabiner.tech>
2023-04-16 14:02:38 +0900

    CI調整

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -8,7 +8,6 @@ Sentry.init({
   dsn: import.meta.env.VITE_SENTRY_DSN,
   integrations: [new Sentry.BrowserTracing()],
   tracesSampleRate: 1.0,
-  release: "my-project-name@2.3.12",
 });
 
 
--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig(({ command, mode }) => {
         project:  env.VITE_SENTRY_PROJECT,
         include: "./dist",
         authToken:  env.VITE_SENTRY_AUTH_TOKEN,
+        release: "my-project-name@2.3.12",
       }),
     ],
   }
